0a7d84c534f448844dfa348cc8fa2582e6a31237
Noemi <noealcasanchez@crackthecode.la>
2025-09-04 02:23:33 -0500

    Clase 28: edición y mejoras en funciones

--- s28/assets/script.js
+++ s28/assets/script.js
@@ -1,16 +1,30 @@
+console.log("FUNCIONES");
 
-function imprimirNumeros() {
 
-  for (let i = 1; i <= 10; i++) {
-    console.log(i);
-  }
+function mostrarMensajes() {
+
+  console.log("Inicio")
+  console.log("Buenos dias");
+  console.log(8);
+  console.log("Noemi");
+  console.log("Fin");
 }
 
-// Llamar a la funcion
-imprimirNumeros();
 
+console.log("Reto 1");
 
-
+function showNumbers() {
+  console.log(1);
+  console.log(2);
+  console.log(3);
+  console.log(4);
+  console.log(5);
+  console.log(6);
+  console.log(7);
+  console.log(8);
+  console.log(9);
+  console.log(10);
+}
 
 
 
@@ -32,6 +46,9 @@ function addTwoNumbers(x, y) {
 
 let num1 = 30;
 let num2 = 8;
+
+console.log("RETORNO DE VALORES")
+
 addTwoNumbers(num1, num2);
 
 
@@ -51,3 +68,20 @@ function despedida (nombre) {
 }
 console.log(despedida('Noemi'));
 console.log(despedida());
+
+console.log("ARROW FUNCTIONS")
+
+let value1 = 12;
+let value2 = 2;
+
+let multiplyTwoNumbers = (a, b) => {
+  console.log(`Factor 1 -> ${a}`);
+  console.log(`Factor 2 -> ${b}`);
+
+  let result = a*b;
+  let message = `${a} x ${b} = ${result}`;
+
+  return message;
+};
+
+console.log(multiplyTwoNumbers(value1, value2));
