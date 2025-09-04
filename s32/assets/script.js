a22968c509ded0e7a059972438942b74c8b585cb
Noemi <noealcasanchez@crackthecode.la>
2025-09-04 01:31:22 -0500

    Clase 32: POO en JS con clases, herencia y polimorfismo

--- s32/assets/script.js
+++ s32/assets/script.js
@@ -1,62 +1,64 @@
-class person { #cellphone;
-    constructor(firstname, lastname, age, heigth, weight, hobbies, cellphone) {
-        this.firstname = firstname;
-        this.lastname = lastname;
-        this.age = age;
-        this.heigth = heigth;
-        this.weight = weight;
-        this.hobbies = hobbies;
-        this.#cellphone = cellphone;
-    }
-    decirAdios() {
-        console.log("Adios desde este metodo!");
-}
-presentar(){
+class Person {
+  #cellphone;
+  constructor(firstname, lastname, age, height, weight, hobbies, cellphone) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.age = age;
+    this.height = height;
+    this.weight = weight;
+    this.hobbies = hobbies;
+    this.#cellphone = cellphone;
+  }
+
+  decirAdios() {
+    console.log("Adiós desde este método!");
+  }
+
+  presentar() {
     console.log(
-        `Adios, me llamo ${this.firstname} ${this.
-        lastname}, tengo ${this.age} años, mido ${this.heigth} y peso ${this.weight} kg. Mis hobbies son: ${this.hobbies}`);
-}
-showCellphone(){
+      `Hola, me llamo ${this.firstname} ${this.lastname}, tengo ${this.age} años, mido ${this.height}m y peso ${this.weight} kg. Mis hobbies son: ${this.hobbies.join(", ")}`
+    );
+  }
+
+  showCellphone() {
     return this.#cellphone;
+  }
+
+  introduce() {
+    console.log(`Soy ${this.firstname} ${this.lastname}, un ser humano normal.`);
+  }
 }
+
+// Herencia
+class Chef extends Person {
+  constructor(firstname, lastname, age, height, weight, hobbies, cellphone, restaurant, favoriteFood) {
+    super(firstname, lastname, age, height, weight, hobbies, cellphone);
+    this.restaurant = restaurant;
+    this.favoriteFood = favoriteFood;
+  }
+
+  cook() {
+    console.log(`${this.firstname} está cocinando ${this.favoriteFood}...`);
+  }
+
+  // Polimorfismo → sobrescribir el método "introduce"
+  introduce() {
+    console.log(`Soy el chef ${this.firstname}, trabajo en ${this.restaurant} y mi comida favorita es ${this.favoriteFood}.`);
+  }
 }
 
-let person1 = new person("Noemi", "Alca", 18, 1.50, 53, [
-    "leer", "jugar", "editar",
-],"981456789");
+// Crear objetos
+let person1 = new Person("Noemi", "Alca", 18, 1.5, 53, ["leer", "jugar", "editar"], "981456789");
+let chef1 = new Chef("Alfredo", "Alca", 18, 1.5, 53, ["cocinar", "comer"], "987654321", "La buena mesa", "pasta");
 
-
-console.log(person1);
-console.log(person1.firstname);
-console.log(person1.hobbies);
-console.log("METODOS");
-person1.decirAdios();
+console.log("---- Persona ----");
 person1.presentar();
-console.log(person1.showCellphone());
+console.log("Celular privado:", person1.showCellphone());
 
-class chef extends person {
-    constructor(
-        firstname, 
-        lastname, 
-        age, 
-        heigth, 
-        weight, 
-        hobbies, 
-        cellphone, 
-        restaurant,
-        favoriteFood
-    ) {
-        super(firstname, lastname, age, heigth, weight, hobbies, cellphone);
-        this.restaurant = restaurant;
-        this.favoriteFood = favoriteFood;
-    }
+console.log("---- Chef ----");
+chef1.presentar();
+chef1.cook();
 
-    cook() {
-        console.log(`${this.firstname} está cocinando ${this.favoriteFood}...`
-
-        );
-    }
-}
-let chef1 = new chef(
-    "Alfredo", "Alca", 18, 1.50, 53, ["cocinar", "comer"], "987654321", "La buena mesa", "pasta"
-)
+console.log("---- Polimorfismo ----");
+person1.introduce(); 
+chef1.introduce(); 
